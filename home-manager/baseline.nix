af72fae92c7489e1a84ae9a31b413fe87da66385
vringar <git@zabka.it>
2026-01-24 21:09:40 +0100

    Finish git

--- home-manager/baseline.nix
+++ home-manager/baseline.nix
@@ -8,7 +8,7 @@
     config,
     ...
   }: {
-    home.packages = [pkgs.atool pkgs.httpie];
+    home.packages = [pkgs.atool pkgs.httpie pkgs.git-cinnabar];
     programs.bash.enable = true;
 
     programs.neovim = {
@@ -28,11 +28,20 @@
       "nix-shell"
       "nix-zsh-completions"
     ];
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = true;
+      enableJujutsuIntegration = true;
+      options = {
+        navigate = true;
+        side-by-side = true;
+      };
+    };
+
+    # TODO: reintroduce nbdime
     programs.git = {
       package = pkgs.gitFull;
       enable = true;
-      settings = {
-      };
       lfs.enable = true;
       signing = {
         signByDefault = true;
@@ -41,6 +50,35 @@
           if (builtins.pathExists "${builtins.toString "/home/${config.home.username}/.ssh/id_ed25519.pub"}")
           then "/home/${config.home.username}/.ssh/id_ed25519.pub"
           else "/home/${config.home.username}/.ssh/github_key.pub";
+      };
+      settings = {
+        core = {
+          editor = lib.getExe config.programs.neovim.finalPackage;
+        };
+        alias = {
+          root = "rev-parse --show-toplevel";
+        };
+        rerere.enable = true;
+        fetch.prune = true;
+        checkout.defaultRemote = "origin";
+
+        sendemail = {
+          smtpserver = "smtp.migadu.com";
+          smtpuser = "git@zabka.it";
+          smtpencryption = "ssl";
+          smptserverport = 465;
+        };
+        # TODO: Figure out how to refer to the git-cinnabar package
+        cinnabar.helper = "git-cinnabar-helper";
+        init.defaultBranch = "main";
+        pull.ff = "only";
+        merge.conflictstyle = "diff3";
+        # TODO: Why explicitly set this to default?
+        diff.colorMoved = "default";
+        push = {
+          default = "upstream";
+          autoSetupRemote = true;
+        };
       };
     };
 
