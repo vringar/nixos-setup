6de92d0413b8366d33e9404f5ebb84ffaf4af3a5
Stefan Zabka <stefan.zabka@camunda.com>
2026-06-29 13:31:15 +0200

    chore: deps

--- apps/rtk/default.nix
+++ apps/rtk/default.nix
@@ -8,7 +8,7 @@ pkgs.rustPlatform.buildRustPackage {
 
   src = sources.rtk;
 
-  cargoHash = "sha256-YsKOyEZ281ojqiitnvCFGy/MzHMyr4hlxqMnvrQwguQ=";
+  cargoHash = "sha256-XKUKdhxfnwUCOx9slqx4oUFa09HcosPLVh5Xkh87oSk=";
 
   nativeBuildInputs = [pkgs.pkg-config];
   buildInputs = [pkgs.sqlite];
