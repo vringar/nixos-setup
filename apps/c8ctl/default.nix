e2fb1b8a3ccd6e301824da3cd6fdacb51407b9a6
vringar <git@zabka.it>
2026-08-23 20:41:40 +0200

    chore(deps): update remaining pins and bump c8ctl
    nixpkgs moves to nixos-unstable 26.11pre1059570.2c423e03bbaf, with
    home-manager, lix-module and lix-src following it. c8ctl goes to
    4.0.0-alpha.5 with a refreshed npmDepsHash.

--- apps/c8ctl/default.nix
+++ apps/c8ctl/default.nix
@@ -1,18 +1,18 @@
 {pkgs}:
 pkgs.buildNpmPackage {
   pname = "c8ctl";
-  version = "4.0.0-alpha.4";
+  version = "4.0.0-alpha.5";
 
   src = pkgs.fetchurl {
-    url = "https://registry.npmjs.org/@camunda8/cli/-/cli-4.0.0-alpha.4.tgz";
-    hash = "sha256-CpzyzwpZ4sQ1vGVEidWudl6kX+PXKezkRYAURMAWe3M=";
+    url = "https://registry.npmjs.org/@camunda8/cli/-/cli-4.0.0-alpha.5.tgz";
+    hash = "sha256-aECDdtY3+IaejNlJ8mNK9sw3l62dKD/yLfzIdc8OKH4=";
   };
   sourceRoot = "package";
 
   nodejs = pkgs.nodejs_22;
   nativeBuildInputs = [pkgs.python3];
   npmDepsFetcherVersion = 2;
-  npmDepsHash = "sha256-lVjDMbkGvFQJLyFtc3a/MatHzCZDS3oUZJXNvDxjZbc=";
+  npmDepsHash = "sha256-e72pFI/RoNR7Oo95ouiGLOhpHnqjWyEnnoRqaYTUOvY=";
   dontNpmBuild = true;
 
   postPatch = ''
